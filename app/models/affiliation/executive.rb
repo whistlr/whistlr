31815260498694161047ff86aab819ba4bf92427
timothythehuman <timothythehuman@gmail.com>
2013-12-11 15:29:24 -0600

    tweaking deployment

--- app/models/affiliation/executive.rb
+++ app/models/affiliation/executive.rb
@@ -1,7 +1,7 @@
 class Affiliation::Executive < Affiliation
 
-  validates :start_date, presence: true, numericality: { only_integer: true }, length: { is: 4 }
-  validates :end_date, allow_nil: true, numericality: { only_integer: true }, length: { is: 4 }
+  #validates :start_date, presence: true, numericality: { only_integer: true }, length: { is: 4 }
+  #validates :end_date, allow_nil: true, numericality: { only_integer: true }, length: { is: 4 }
   validates :title, presence: true
 
 end
--- app/models/affiliation/owner.rb
+++ app/models/affiliation/owner.rb
@@ -1,5 +1,5 @@
 class Affiliation::Owner < Affiliation
 
-  validates :shares, presence: true, numericality: {less_than_or_equal_to: 100}
+  # validates :shares, presence: true, numericality: {less_than_or_equal_to: 100}
 
 end
